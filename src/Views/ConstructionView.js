0a7ffa94fa4988f701dbe3c0bf2f1caf86d685f2
MatthewLamperski <matthew.lamperski@gmail.com>
2021-12-16 20:50:36 -0500

    Changed ConstructionView

--- src/Views/ConstructionView.js
+++ src/Views/ConstructionView.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import '../index.css'
 import {Box, Text, Fade, Divider} from "native-base";
-import { FaHammer } from "react-icons/all";
 
 const ConstructionView = () => {
   const [constructionText, setConstructionText] = useState("Wadzoo is currently under construction")
@@ -9,7 +8,7 @@ const ConstructionView = () => {
     setInterval(() => {
       setConstructionText(prevState => {
         if (prevState.substr(prevState.length - 3) === '...') {
-          return "Wadzoo is currently under construction"
+          return "We are currently under construction"
         } else {
           return prevState + "."
         }
@@ -35,12 +34,33 @@ const ConstructionView = () => {
             w={"75%"}
             p={10}
             rounded={8}
+            shadow={5}
           >
             <Text
               fontWeight={300}
-              fontSize={30}
+              fontSize={35}
+              textAlign="center"
+            >Wadzoo</Text>
+            <Text
+              pt={2}
+              fontWeight={300}
+              fontSize={20}
+              textAlign="center"
+            >Connect with other real estate investors. Expand your network. Find exclusive listings.</Text>
+            <Divider m={4} bg="black" />
+            <Text
+              fontWeight={300}
+              fontSize={15}
+              textAlign="center"
             >{constructionText}</Text>
           </Box>
+        </Fade>
+        <Fade in={true} duration={1000}>
+          <Text
+            fontWeight={300}
+            fontSize={20}
+            textAlign="center"
+          >Coming soon to the App Store and Google Play Store.</Text>
         </Fade>
       </Box>
     </div>
