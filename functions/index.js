54147e6cf8d7759a9db7c5f8feb3a1fe48f281d4
MatthewLamperski <matthew.lamperski@gmail.com>
2022-03-27 20:52:00 -0400

    Began Plaid Implementation

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const {Configuration, PlaidEnvironments, PlaidApi} = require("plaid");
+const cors = require("cors")({origin: true});
+require("dotenv").config();
 admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello Wadzoo user, you have found our API...");
 });
 
@@ -15,64 +18,113 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  */
 
 exports.addUsersCount = functions.firestore
-  .document("users/{uid}")
-  .onCreate((snap, context) => {
-    admin
-      .firestore()
-      .doc("appInfo")
-      .update({ usersCount: admin.firestore.FieldValue.increment(1) })
-      .then(() => {
-        functions.logger.log("Added user, updated appInfo");
-      })
-      .catch((err) => {
-        functions.logger.log("Error occured", err);
-      });
-  });
+    .document("users/{uid}")
+    .onCreate((snap, context) => {
+      admin
+          .firestore()
+          .doc("app/appInfo")
+          .update({usersCount: admin.firestore.FieldValue.increment(1)})
+          .then(() => {
+            functions.logger.log("Added user, updated appInfo");
+          })
+          .catch((err) => {
+            functions.logger.log("Error occured", err);
+          });
+    });
 
 exports.deleteUsersCount = functions.firestore
-  .document("users/{uid}")
-  .onDelete((snap, context) => {
-    admin
-      .firestore()
-      .doc("appInfo")
-      .update({ usersCount: admin.firestore.FieldValue.increment(-1) })
-      .then(() => {
-        functions.logger.log("Deleted user doc, updated appInfo");
-      })
-      .catch((err) => {
-        functions.logger.log("Error occured", err);
-      });
-  });
+    .document("users/{uid}")
+    .onDelete((snap, context) => {
+      admin
+          .firestore()
+          .doc("app/appInfo")
+          .update({usersCount: admin.firestore.FieldValue.increment(-1)})
+          .then(() => {
+            functions.logger.log("Deleted user doc, updated appInfo");
+          })
+          .catch((err) => {
+            functions.logger.log("Error occured", err);
+          });
+    });
 
 exports.addListingsCount = functions.firestore
-  .document("listings/{docID}")
-  .onCreate((snap, context) => {
-    admin
-      .firestore()
-      .doc("appInfo")
-      .update({ listingsCount: admin.firestore.FieldValue.increment(1) })
-      .then(() => {
-        functions.logger.log("Added listing, updated appInfo");
-      })
-      .catch((err) => {
-        functions.logger.log("Error occured", err);
-      });
-  });
+    .document("listings/{docID}")
+    .onCreate((snap, context) => {
+      admin
+          .firestore()
+          .doc("app/appInfo")
+          .update({listingsCount: admin.firestore.FieldValue.increment(1)})
+          .then(() => {
+            functions.logger.log("Added listing, updated appInfo");
+          })
+          .catch((err) => {
+            functions.logger.log("Error occured", err);
+          });
+    });
 
 exports.deleteListingsCount = functions.firestore
-  .document("listings/{docID}")
-  .onDelete((snap, context) => {
-    admin
-      .firestore()
-      .doc("appInfo")
-      .update({ listingsCount: admin.firestore.FieldValue.increment(-1) })
-      .then(() => {
-        functions.logger.log("Deleted listing doc, updated appInfo");
-      })
-      .catch((err) => {
-        functions.logger.log("Error occured", err);
-      });
+    .document("listings/{docID}")
+    .onDelete((snap, context) => {
+      admin
+          .firestore()
+          .doc("app/appInfo")
+          .update({listingsCount: admin.firestore.FieldValue.increment(-1)})
+          .then(() => {
+            functions.logger.log("Deleted listing doc, updated appInfo");
+          })
+          .catch((err) => {
+            functions.logger.log("Error occured", err);
+          });
+    });
+
+/**
+ * This function will generate a link token for Plaid
+ */
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET,
+    },
+  },
+});
+const client = new PlaidApi(configuration);
+
+exports.testEnv = functions.https.onRequest((req, res) => {
+  res.send(
+      `Here is what the environment variables returned: 
+      ${process.env.TEST1} ${process.env.TEST2}`
+  );
+});
+
+exports.generateLinkToken = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    functions.logger.log("NEW PLAID REQUEST", request);
+    if (request.method === "POST") {
+      const clientUID = request.body.uid;
+      const plaidRequest = {
+        user: {
+          client_user_id: clientUID,
+        },
+        client_name: "Wadzoo",
+        products: ["auth"],
+        language: "en",
+        country_codes: ["US"],
+      };
+      client
+          .linkTokenCreate(plaidRequest)
+          .then((createTokenResponse) => {
+            response.json(createTokenResponse.data);
+          })
+          .catch((err) => {
+            functions.logger.log("ERROR", err);
+            response.json(err);
+          });
+    }
   });
+});
 
 /**
  * This function receives a webhook from IAPHUB,
@@ -81,81 +133,83 @@ exports.deleteListingsCount = functions.firestore
  */
 
 exports.iaphubSubscriptionNoty = functions.https.onRequest(
-  (request, response) => {
-    functions.logger.log(
-      "NEW IAPHUB REQUEST, METHOD:",
-      request.method,
-      request
-    );
-    if (request.method === "POST") {
-      admin
-        .firestore()
-        .doc("users/aGMk6uiO7OPpaMbhKLR1qG3h5Xm2")
-        .get()
-        .then((doc) => {
-          const data = doc.data();
-          if ("notificationToken" in data) {
-            const token = data.notificationToken.token;
-            let notification = {
-              title: "An IAPHUB Webhook was hit...",
-              body: `${request.body.type}`,
-            };
-            switch (request.body.type) {
-              case "purchase":
-                notification = {
-                  title: "A user has bought a new subscription!",
-                  // eslint-disable-next-line max-len
-                  body: `A new ${request.body.data.productSku.trim()} subscription has been purchased`,
+    (request, response) => {
+      functions.logger.log(
+          "NEW IAPHUB REQUEST, METHOD:",
+          request.method,
+          request,
+          "PLAID_ENV",
+          process.env.PLAID_ENV
+      );
+      if (request.method === "POST") {
+        admin
+            .firestore()
+            .doc("users/aGMk6uiO7OPpaMbhKLR1qG3h5Xm2")
+            .get()
+            .then((doc) => {
+              const data = doc.data();
+              if ("notificationToken" in data) {
+                const token = data.notificationToken.token;
+                let notification = {
+                  title: "An IAPHUB Webhook was hit...",
+                  body: `${request.body.type}`,
                 };
-                break;
-              case "subscription_renewal":
-                notification = {
-                  title: "A user has renewed their subscription!",
-                  // eslint-disable-next-line max-len
-                  body: `A ${request.body.data.productSku.trim()} subscription has been renewed.`,
+                switch (request.body.type) {
+                  case "purchase":
+                    notification = {
+                      title: "A user has bought a new subscription!",
+                      // eslint-disable-next-line max-len
+                      body: `A new ${request.body.data.productSku.trim()} subscription has been purchased`,
+                    };
+                    break;
+                  case "subscription_renewal":
+                    notification = {
+                      title: "A user has renewed their subscription!",
+                      // eslint-disable-next-line max-len
+                      body: `A ${request.body.data.productSku.trim()} subscription has been renewed.`,
+                    };
+                    break;
+                  case "subscription_cancel":
+                    notification = {
+                      title: "A user has canceled their subscription.",
+                      // eslint-disable-next-line max-len
+                      body: `A ${request.body.data.productSku.trim()} subscription has been cancelled.`,
+                    };
+                    break;
+                  case "subscription_expire":
+                    notification = {
+                      title: "A user's subscription has expired.",
+                      // eslint-disable-next-line max-len
+                      body: `A ${request.body.data.productSku.trim()} subscription has just expired.`,
+                    };
+                    break;
+                }
+                const payload = {
+                  notification: {
+                    title: notification.title,
+                    body: notification.body,
+                    sound: "default",
+                  },
                 };
-                break;
-              case "subscription_cancel":
-                notification = {
-                  title: "A user has canceled their subscription.",
-                  // eslint-disable-next-line max-len
-                  body: `A ${request.body.data.productSku.trim()} subscription has been cancelled.`,
-                };
-                break;
-              case "subscription_expire":
-                notification = {
-                  title: "A user's subscription has expired.",
-                  // eslint-disable-next-line max-len
-                  body: `A ${request.body.data.productSku.trim()} subscription has just expired.`,
-                };
-                break;
-            }
-            const payload = {
-              notification: {
-                title: notification.title,
-                body: notification.body,
-                sound: "default",
-              },
-            };
-            admin
-              .messaging()
-              .sendToDevice(token, payload)
-              .then((value) => {
-                functions.logger.log("Send subscription noty", value);
-              })
-              .catch((err) => {
-                functions.logger.log("Couldn't send sub noty", err);
-              });
-          } else {
-            functions.logger.log("No notification token in user doc");
-          }
-        })
-        .catch((err) => {
-          functions.logger.log("Couldn't get your user doc Matthew.", err);
-        });
-      response.sendStatus(200);
+                admin
+                    .messaging()
+                    .sendToDevice(token, payload)
+                    .then((value) => {
+                      functions.logger.log("Send subscription noty", value);
+                    })
+                    .catch((err) => {
+                      functions.logger.log("Couldn't send sub noty", err);
+                    });
+              } else {
+                functions.logger.log("No notification token in user doc");
+              }
+            })
+            .catch((err) => {
+              functions.logger.log("Couldn't get your user doc Matthew.", err);
+            });
+        response.sendStatus(200);
+      }
     }
-  }
 );
 
 /**
@@ -165,58 +219,58 @@ exports.iaphubSubscriptionNoty = functions.https.onRequest(
  */
 
 exports.sendMessageNotification = functions.firestore
-  .document("users/{currentUID}/chats/{messagingUID}")
-  .onWrite((change, context) => {
-    functions.logger.log(
-      "context.params:",
-      context.params,
-      "change.after.data()",
-      change.after.data()
-    );
-    const messagingName = change.after.data().displayName;
-    const lastMessageText = change.after.data().text;
-    const outgoing = change.after.data().outgoing;
-    const profilePicURL = change.after.data().profilePicURL
-      ? change.after.data().profilePicURL
-      : "none";
-    if (!outgoing) {
-      admin
-        .firestore()
-        .doc("users/" + context.params.currentUID)
-        .get()
-        .then((doc) => {
-          const data = doc.data();
-          if ("notificationToken" in data) {
-            const token = data.notificationToken.token;
-            const payload = {
-              notification: {
-                title: messagingName,
-                body: lastMessageText,
-                sound: "default",
-              },
-              data: {
-                type: "message",
-                profilePicURL: profilePicURL,
-              },
-            };
-            admin
-              .messaging()
-              .sendToDevice(token, payload)
-              .then((value) => {
-                functions.logger.log(
-                  "Successfully sent user notification",
-                  value
-                );
-              })
-              .catch((err) => {
-                functions.logger.log("Error sending notification", err);
-              });
-          } else {
-            functions.logger.log("No notificationToken Obj in user doc");
-          }
-        })
-        .catch((err) => {
-          functions.logger.log("Could not fetch user document", err);
-        });
-    }
-  });
+    .document("users/{currentUID}/chats/{messagingUID}")
+    .onWrite((change, context) => {
+      functions.logger.log(
+          "context.params:",
+          context.params,
+          "change.after.data()",
+          change.after.data()
+      );
+      const messagingName = change.after.data().displayName;
+      const lastMessageText = change.after.data().text;
+      const outgoing = change.after.data().outgoing;
+      const profilePicURL = change.after.data().profilePicURL ?
+      change.after.data().profilePicURL :
+      "none";
+      if (!outgoing) {
+        admin
+            .firestore()
+            .doc("users/" + context.params.currentUID)
+            .get()
+            .then((doc) => {
+              const data = doc.data();
+              if ("notificationToken" in data) {
+                const token = data.notificationToken.token;
+                const payload = {
+                  notification: {
+                    title: messagingName,
+                    body: lastMessageText,
+                    sound: "default",
+                  },
+                  data: {
+                    type: "message",
+                    profilePicURL: profilePicURL,
+                  },
+                };
+                admin
+                    .messaging()
+                    .sendToDevice(token, payload)
+                    .then((value) => {
+                      functions.logger.log(
+                          "Successfully sent user notification",
+                          value
+                      );
+                    })
+                    .catch((err) => {
+                      functions.logger.log("Error sending notification", err);
+                    });
+              } else {
+                functions.logger.log("No notificationToken Obj in user doc");
+              }
+            })
+            .catch((err) => {
+              functions.logger.log("Could not fetch user document", err);
+            });
+      }
+    });
--- src/Routes/Checkout.js
+++ src/Routes/Checkout.js
@@ -1,19 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-  Image,
-  PresenceTransition,
-  Pressable,
-  Spinner,
-  Text,
-  useTheme,
-} from "native-base";
-import { Col, Container, Row } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Button, PresenceTransition, Text, useTheme } from "native-base";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import { AppContext } from "../AppContext";
-import { FaArrowCircleRight } from "react-icons/all";
-import AccessDenied from "../Views/AccessDenied";
 import LoadingScreen from "../Views/LoadingScreen";
 import PortalAuth from "./PortalAuth";
+import { deviceType } from "./LandingPage";
+import "./Checkout.css";
+import { FaCheck } from "react-icons/all";
+import { usePlaidLink } from "react-plaid-link";
+
+const headers = {
+  "Content-Type": "application/json",
+  Accept: "application.json",
+};
 
 const Checkout = ({ setNavbarTransparent }) => {
   const [navbarHeight, setnavbarHeight] = useState();
@@ -23,9 +23,92 @@ const Checkout = ({ setNavbarTransparent }) => {
       document.getElementsByClassName("navbar").item(0).clientHeight
     );
   }, []);
+
   const { service } = useParams();
   const { user } = useContext(AppContext);
   const theme = useTheme();
+  const [step, setStep] = useState(1);
+  const [completedSteps, setCompletedSteps] = useState([]);
+  const [linkToken, setLinkToken] = useState(null);
+  const generateToken = async () => {
+    console.log(user.uid);
+    const response = await fetch("https://wadzoo.com/api/create_link_token", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({
+        uid: user.uid,
+      }),
+    });
+    const data = await response.json();
+    setLinkToken(data.link_token);
+    console.log("PLAID DATA", data);
+  };
+  useEffect(() => {
+    if (user) {
+      generateToken();
+    }
+  }, [user]);
+  const hideOnMobile = () => {
+    return deviceType() === "iOS" || deviceType() === "android";
+  };
+  const StepOne = () => {
+    return (
+      <PresenceTransition
+        visible={step === 1}
+        initial={{ opacity: 0, translateX: 30 }}
+        animate={{ opacity: 1, translateX: 0, transition: { duration: 300 } }}
+      >
+        <div className="d-flex flex-column justify-content-start align-items-start py-4">
+          <Text fontWeight={300} fontSize={22} color="secondary.800">
+            Step 1
+          </Text>
+          <Text fontSize={18} color="muted.400">
+            Verify funding of at least $
+            {service === "verifyAdvanced" ? "500" : "300"}k with Plaid
+          </Text>
+          <PlaidLink linkToken={linkToken} />
+        </div>
+      </PresenceTransition>
+    );
+  };
+  const StepTwo = () => {
+    return (
+      <PresenceTransition
+        visible={step === 2}
+        initial={{ opacity: 0, translateX: 30 }}
+        animate={{ opacity: 1, translateX: 0, transition: { duration: 300 } }}
+      >
+        <div className="d-flex flex-column justify-content-start align-items-start py-4">
+          <Text fontWeight={300} fontSize={22} color="secondary.800">
+            Step 2
+          </Text>
+          <Text fontSize={18} color="muted.400">
+            Verify you have completed{" "}
+            {service === "verifyAdvanced" ? "50+" : "10 - 15"} deals
+          </Text>
+        </div>
+      </PresenceTransition>
+    );
+  };
+
+  const StepThree = () => {
+    return (
+      <PresenceTransition
+        visible={step === 3}
+        initial={{ opacity: 0, translateX: 30 }}
+        animate={{ opacity: 1, translateX: 0, transition: { duration: 300 } }}
+      >
+        <div className="d-flex flex-column justify-content-start align-items-start py-4">
+          <Text fontWeight={300} fontSize={22} color="secondary.800">
+            Step 3
+          </Text>
+          <Text fontSize={18} color="muted.400">
+            Sumbit your information for verification.
+          </Text>
+        </div>
+      </PresenceTransition>
+    );
+  };
   if (user) {
     return (
       <PresenceTransition
@@ -44,24 +127,176 @@ const Checkout = ({ setNavbarTransparent }) => {
           }}
         >
           <Container
-            className="my-5 p-4"
+            className="mt-5 mb-3 p-4"
             style={{
               flexDirection: "column",
               justifyContent: "flex-start",
               alignItems: "flex-start",
               display: "flex",
-              backgroundColor: theme.colors.primary["500"],
-              background: `linear-gradient(-50deg, ${theme.colors.primary["400"]}, ${theme.colors.secondary["400"]})`,
               borderRadius: 8,
-              boxShadow:
-                "0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)",
             }}
           >
-            <Text fontSize={20}>Welcome, {user.firstName}</Text>
-            <Text>
-              Let's get your account verified for the{" "}
-              {service === "verifyAdvanced" ? "Advanced" : "Intermediate"} Badge
+            <Text fontWeight={300} color="secondary.800" fontSize={22}>
+              Welcome, {user.firstName}
             </Text>
+            <Text fontSize={18} color="muted.400">
+              Let's get your account verified for the{" "}
+              {service === "verifyAdvanced" ? "Advanced" : "Intermediate"}{" "}
+              Badge.
+            </Text>
+          </Container>
+          <Container className="d-flex flex-column justify-content-center align-items-start flex-row px-4">
+            <div
+              className="d-flex flex-row align-items-center justify-content-between p-3"
+              style={{
+                width: "100%",
+                borderRadius: 8,
+                backgroundColor: theme.colors.primary["50"] + "70",
+              }}
+            >
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => setStep(1)}
+                className="d-flex flex-row align-items-center px-3"
+              >
+                <div
+                  className="p-2 circle"
+                  style={{
+                    backgroundColor:
+                      completedSteps.includes(1) || step === 1
+                        ? theme.colors.primary["400"]
+                        : "white",
+                    transition: "all .4s ease",
+                    WebkitTransition: "all .4s ease",
+                    MozTransition: "all .4s ease",
+                  }}
+                >
+                  <span
+                    style={{
+                      color:
+                        completedSteps.includes(1) || step === 1
+                          ? "white"
+                          : theme.colors.primary["400"],
+                      transition: "all .4s ease",
+                      WebkitTransition: "all .4s ease",
+                      MozTransition: "all .4s ease",
+                    }}
+                  >
+                    {completedSteps.includes(1) ? (
+                      <FaCheck color="white" />
+                    ) : (
+                      "1"
+                    )}
+                  </span>
+                </div>
+                {!hideOnMobile() && (
+                  <Text pl={2} color="primary.700">
+                    Funding
+                  </Text>
+                )}
+              </div>
+              <div
+                className="flex-grow-1"
+                style={{
+                  backgroundColor: theme.colors.primary["400"],
+                  borderRadius: 3,
+                  height: 2,
+                }}
+              />
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => setStep(2)}
+                className="d-flex flex-row align-items-center px-3"
+              >
+                <div
+                  className="p-2 circle"
+                  style={{
+                    backgroundColor:
+                      completedSteps.includes(2) || step === 2
+                        ? theme.colors.primary["400"]
+                        : "white",
+                    transition: "all .4s ease",
+                    WebkitTransition: "all .4s ease",
+                    MozTransition: "all .4s ease",
+                  }}
+                >
+                  <span
+                    style={{
+                      color:
+                        completedSteps.includes(2) || step === 2
+                          ? "white"
+                          : theme.colors.primary["400"],
+                      transition: "all .4s ease",
+                      WebkitTransition: "all .4s ease",
+                      MozTransition: "all .4s ease",
+                    }}
+                  >
+                    {completedSteps.includes(2) ? (
+                      <FaCheck color="white" />
+                    ) : (
+                      "2"
+                    )}
+                  </span>
+                </div>
+                {!hideOnMobile() && (
+                  <Text pl={2} color="primary.700">
+                    Deals
+                  </Text>
+                )}
+              </div>
+              <div
+                className="flex-grow-1"
+                style={{
+                  backgroundColor: theme.colors.primary["400"],
+                  borderRadius: 3,
+                  height: 2,
+                }}
+              />
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => setStep(3)}
+                className="d-flex flex-row align-items-center px-3"
+              >
+                <div
+                  className="p-2 circle"
+                  style={{
+                    backgroundColor:
+                      completedSteps.includes(3) || step === 3
+                        ? theme.colors.primary["400"]
+                        : "white",
+                    transition: "all .4s ease",
+                    WebkitTransition: "all .4s ease",
+                    MozTransition: "all .4s ease",
+                  }}
+                >
+                  <span
+                    style={{
+                      color:
+                        completedSteps.includes(3) || step === 3
+                          ? "white"
+                          : theme.colors.primary["400"],
+                      transition: "all .4s ease",
+                      WebkitTransition: "all .4s ease",
+                      MozTransition: "all .4s ease",
+                    }}
+                  >
+                    {completedSteps.includes(3) ? (
+                      <FaCheck color="white" />
+                    ) : (
+                      "3"
+                    )}
+                  </span>
+                </div>
+                {!hideOnMobile() && (
+                  <Text pl={2} color="primary.700">
+                    Submit
+                  </Text>
+                )}
+              </div>
+            </div>
+            <StepOne />
+            <StepTwo />
+            <StepThree />
           </Container>
         </div>
       </PresenceTransition>
@@ -76,6 +311,21 @@ const Checkout = ({ setNavbarTransparent }) => {
   } else {
     return <LoadingScreen setNavbarTransparent={setNavbarTransparent} />;
   }
+};
+
+const PlaidLink = ({ linkToken }) => {
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+      console.log("PT", public_token);
+      console.log(metadata);
+    },
+  });
+  return (
+    <Button onPress={() => open()} isDisabled={!ready}>
+      <Text button>Connect a bank account</Text>
+    </Button>
+  );
 };
 
 export default Checkout;
